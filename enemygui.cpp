62b3740cbc739e85563b908d20ff806918ed70bf
jalagaban <jalagaban@gmail.com>
2013-11-04 21:10:45 -0500

    Cleaned up updateDirection method EnemyGUI to support Sergeant and YOLO units

--- enemygui.cpp
+++ enemygui.cpp
@@ -1,5 +1,5 @@
 #include "enemygui.h"
-
+//Create enemy image
 EnemyGUI::EnemyGUI(MainWindow* parent, Enemy* newEn, QWidget *newWorld) : QLabel(newWorld)
 {
     win = parent;
@@ -19,19 +19,52 @@ EnemyGUI::EnemyGUI(MainWindow* parent, Enemy* newEn, QWidget *newWorld) : QLabel
     }
 
 }
-
+//Change enemy image based on enemy direction
 void EnemyGUI::updateDirection()
 {
-    if (enemy->getDirection() == "right")
+    if (enemy->getType() == "walker")
     {
-        pic.load(":/walkerRight.png");
+        if (enemy->getDirection() == "right")
+        {
+            pic.load(":/walkerRight.png");
+        }
+        else if (enemy->getDirection() == "down")
+        {
+            pic.load(":/walkerDown.png");
+        }
+        else if (enemy->getDirection() == "left")
+        {
+            pic.load(":/walkerLeft.png");
+        }
     }
-    else if (enemy->getDirection() == "down")
+    else if (enemy->getType() == "sergeant")
     {
-        pic.load(":/walkerDown.png");
+        if (enemy->getDirection() == "right")
+        {
+            pic.load(":/SergeantRight.png");
+        }
+        else if (enemy->getDirection() == "down")
+        {
+            pic.load(":/SergeantDown.png");
+        }
+        else if (enemy->getDirection() == "left")
+        {
+            pic.load(":/SergeantLeft.png");
+        }
     }
-    else if (enemy->getDirection() == "left")
+    else if (enemy->getType() == "YOLO")
     {
-        pic.load(":/walkerLeft.png");
+        if (enemy->getDirection() == "right")
+        {
+            pic.load(":/YOLORight.png");
+        }
+        else if (enemy->getDirection() == "down")
+        {
+            pic.load(":/YOLODown.png");
+        }
+        else if (enemy->getDirection() == "left")
+        {
+            pic.load(":/YOLOLeft.png");
+        }
     }
 }
